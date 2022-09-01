0c89153c2949831149dc5f5bd744eac3896afdf9
Adam Boguszewski <aboguszewski@sumologic.com>
2022-09-01 13:37:23 +0200

    feat(tests/integration): add generating traces for tests

---
+++ tests/integration/helm_otelcol_traces_test.go
@@ -0,0 +1,129 @@
+package integration
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/SumoLogic/sumologic-kubernetes-collection/tests/integration/internal/ctxopts"
+	terrak8s "github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/e2e-framework/klient/k8s"
+	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
+	"sigs.k8s.io/e2e-framework/klient/wait"
+	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	"sigs.k8s.io/e2e-framework/pkg/features"
+)
+
+func Test_Helm_Otelcol_Traces(t *testing.T) {
+	const (
+		tickDuration         = 3 * time.Second
+		waitDuration         = 3 * time.Minute
+		tracesCount     uint = 4 // number of traces generated
+		spansPerTrace   uint = 500
+		totalSpansCount uint = tracesCount * spansPerTrace
+	)
+	featInstall := features.New("traces").
+		Assess("sumologic secret is created with endpoints",
+			func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
+				terrak8s.WaitUntilSecretAvailable(t, ctxopts.KubectlOptions(ctx), "sumologic", 60, tickDuration)
+				secret := terrak8s.GetSecret(t, ctxopts.KubectlOptions(ctx), "sumologic")
+				require.Len(t, secret.Data, 2, "Secret has incorrect number of endpoints. There should be only 1 endpoint, for logs.")
+				return ctx
+			}).
+		// TODO: Rewrite into similar step func as WaitUntilStatefulSetIsReady but for deployments
+		Assess("otelcol deployment is ready", func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
+			res := envConf.Client().Resources(ctxopts.Namespace(ctx))
+			releaseName := ctxopts.HelmRelease(ctx)
+			labelSelector := fmt.Sprintf("app=%s-sumologic-otelcol", releaseName)
+			ds := appsv1.DeploymentList{}
+
+			require.NoError(t,
+				wait.For(
+					conditions.New(res).
+						ResourceListN(&ds, 1,
+							resources.WithLabelSelector(labelSelector),
+						),
+					wait.WithTimeout(waitDuration),
+					wait.WithInterval(tickDuration),
+				),
+			)
+			require.NoError(t,
+				wait.For(
+					conditions.New(res).
+						DeploymentConditionMatch(&ds.Items[0], appsv1.DeploymentAvailable, corev1.ConditionTrue),
+					wait.WithTimeout(waitDuration),
+					wait.WithInterval(tickDuration),
+				),
+			)
+			return ctx
+		}).
+		// TODO: Rewrite into similar step func as WaitUntilStatefulSetIsReady but for daemonsets
+		Assess("otelagent daemonset is ready", func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
+			res := envConf.Client().Resources(ctxopts.Namespace(ctx))
+			nl := corev1.NodeList{}
+			if !assert.NoError(t, res.List(ctx, &nl)) {
+				return ctx
+			}
+
+			releaseName := ctxopts.HelmRelease(ctx)
+			labelSelector := fmt.Sprintf("app=%s-sumologic-otelagent", releaseName)
+			ds := appsv1.DaemonSetList{}
+
+			require.NoError(t,
+				wait.For(
+					conditions.New(res).
+						ResourceListN(&ds, 1,
+							resources.WithLabelSelector(labelSelector),
+						),
+					wait.WithTimeout(waitDuration),
+					wait.WithInterval(tickDuration),
+				),
+			)
+			require.NoError(t,
+				wait.For(
+					conditions.New(res).
+						ResourceMatch(&ds.Items[0], func(object k8s.Object) bool {
+							d := object.(*appsv1.DaemonSet)
+							return d.Status.NumberUnavailable == 0 &&
+								d.Status.NumberReady == int32(len(nl.Items))
+						}),
+					wait.WithTimeout(waitDuration),
+					wait.WithInterval(tickDuration),
+				),
+			)
+			return ctx
+		}).Feature()
+
+	featTraces := features.New("traces").
+		Setup(func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
+			// TODO: This should be refactored. See internal/stepfuncs/logs.go for inspirations.
+			release := ctxopts.HelmRelease(ctx)
+
+			opts := ctxopts.KubectlOptions(ctx)
+
+			colName := fmt.Sprintf("%s-sumologic-otelcol", release)
+			args := []string{
+				"run", "cst",
+				"--image", "localhost:5001/kubernetes-tools:dev-latest",
+				fmt.Sprintf("--serviceaccount=%s-sumologic", release),
+				"--env", "TOTAL_TRACES=2",
+				"--env", "SPANS_PER_TRACE=2",
+				"--env", fmt.Sprintf("COLLECTOR_HOSTNAME=%s", colName),
+				"--",
+				"customer-trace-tester",
+			}
+			terrak8s.RunKubectl(t, opts, args...)
+			return ctx
+		}).Assess("wait for traces", func(c context.Context, _ *testing.T, _ *envconf.Config) context.Context {
+		time.Sleep(120 * time.Second)
+		return c
+	}).Feature()
+
+	testenv.Test(t, featInstall, featTraces)
+}
